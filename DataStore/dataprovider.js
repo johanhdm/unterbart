f2106cfdbeaa7a9d93b9d83e91fc7e125ba7593d
Johan Halvarsson de Maar <johanhdm@Johans-iMac.local>
2013-10-02 22:40:48 +0200

    init commit

--- DataStore/dataprovider.js
+++ DataStore/dataprovider.js
@@ -1,66 +1,35 @@
 var redis = require('redis')
 	, client = redis.createClient();
 
-//init
-client.on("subscribe", function(channel, count){
-	console.log("subscribing to:", channel);
-});
-
-client.on("message", function(channel, message){
-	//save message to all boards 
-
-	console.log("IMPORTANT MESSAGE: ", channel, " - ", message);
-});
 
 
-exports.getBoardById = function(id, callback){
-	if (id){
-		client.zrevrange(id, 0 , -1, function(err, data){
-			if (err){
-				console.log(err);
-			}
-			else{
-				console.log(data);
-			}
-		});	
-	}
-	else{
-		callback("Board id cannot be empty", null);
-	}
-}
+exports.addPostToBoards = function(post, tag, callback){
 
-
-
-exports.saveBoardSettings = function(board, callback){
-
-	//client.subscribe('tag');
-	callback();
-
-	//callback(null, board);
-
-	//for each tag in board, subscribe to the tag's channel - if tag has no channel - create a channel
+	getBoardsWithTag(tag, function(err, boards){
+		for (var i = boards.length - 1; i >= 0; i--) {
+			client.zadd( boards[i], 1, post); 
+		};
+		callback();
+	});
 
 };
 
-exports.addTagToBoard = function(tag, board, callback){
-	
-	//if there are no subscriptions to this tag
-	client.subscribe(tag);
+exports.addBoardToTag = function(boardId, tag, callback){
+	client.sadd(tag, boardId);
+	callback();
+};
 
-	//tag = set of boards
-	//add board to tag set
-	client.sadd(tag, board);
-
-
+exports.getPostsForBoard = function(boardId, callback){
+	client.zrevrange(boardId, 0, -1, function(err, data){
+		callback(err, data);
+	});
 };
 
 
-//called by the endpoints
-exports.savePostToTag = function(post, tag, callback){
-
-	console.log(tag, " --- ", post);
-	client.publish(tag, post);
-	callback();
+function getBoardsWithTag(tag, callback){
+	client.smembers(tag, function(err, obj){
+		callback(err, obj);
+	});
 
 }
 
--- Endpoints/server.js
+++ Endpoints/server.js
@@ -2,12 +2,19 @@ var restify = require('restify')
 	, dataclient = require('../DataStore/dataprovider.js');
 
 
-function respond(req, res, next) {
-	dataclient.savePostToTag(req.params.post, req.params.tag, function(){
-		console.log('respond done');
+function addPost(req, res, next) {
+
+	dataclient.addPostToBoards(req.params.post, req.params.tag, function(err, data){
+		console.log('respons done');
 		res.send('ok');
 	});
+}
 
+function addBoardToTag(req, res, next){
+	dataclient.addBoardToTag(req.params.board, req.params.tag, function(){
+		console.log('respons done');
+		res.send('ok');
+	});
 }
 
 var server = restify.createServer({
@@ -21,8 +28,20 @@ var server = restify.createServer({
 });
 
 //routes
-server.get('/:tag/:post', respond);
-server.head('/board/:id', respond);
+server.get('/tags/:tag/:post', addPost);
+server.get('/board/:tag/:board', addBoardToTag);
+
+
+//actually API stuff
+server.get('/:board', getPostsByBoard);
+
+function getPostsByBoard(req, res, next){
+	dataclient.getPostsForBoard(req.params.board, function(err, data){
+		console.log(data);
+		res.send(data);
+	});
+
+};
 
 
 server.listen(4000, function() {
